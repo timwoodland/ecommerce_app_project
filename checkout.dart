67471b9deedd1456b4f2c37cf21f7ea750b87e06
timwoodland <timwoodland@me.com>
2024-11-24 22:06:13 +0000

    checkout code written

--- checkout.dart
+++ checkout.dart
@@ -1,1 +1,142 @@
+import 'dart:io';
 
+class Product {
+  final int id;
+  final String name;
+  final double price;
+
+  const Product({required this.id, required this.name, required this.price});
+
+  String get diplayName => '($initial)${name.substring(1)}: \$$price';
+  String get initial => name.substring(0, 1);
+}
+
+class Item {
+  final Product product;
+  final int quantity;
+
+  const Item({required this.product, this.quantity = 1});
+
+  double get price => quantity * product.price;
+
+  @override
+  String toString() {
+    return '$quantity x ${product.name}: \$$price';
+  }
+}
+
+class Cart {
+  final Map<int, Item> _items = {};
+
+  void addProduct(Product product) {
+    final item = _items[product.id];
+    if (item == null) {
+      _items[product.id] = Item(product: product, quantity: 1);
+    } else {
+      _items[product.id] = Item(product: product, quantity: item.quantity + 1);
+    }
+  }
+
+  bool get isEmpty => _items.isEmpty;
+
+  double total() {
+    return _items.values
+        .map((item) => item.price)
+        .reduce((value, element) => value + element);
+  }
+
+  @override
+  String toString() {
+    if (_items.isEmpty) {
+      return 'Cart is empty';
+    }
+    final itemisedList =
+        _items.values.map((item) => item.toString()).join('\n');
+    return '------\n$itemisedList\nTotal: \$${total()}\n------';
+  }
+}
+
+const allProducts = [
+  Product(id: 1, name: 'apples', price: 1.60),
+  Product(id: 2, name: 'bananas', price: 0.70),
+  Product(id: 3, name: 'courgettes', price: 1.0),
+  Product(id: 4, name: 'grapes', price: 2.0),
+  Product(id: 5, name: 'mushrooms', price: 0.8),
+  Product(id: 6, name: 'potatoes', price: 1.5),
+];
+
+// Sudo code
+// loop
+//  user promp: view cart / add item / checkout
+//  if select == add item:
+//    choose product
+//    add it to the cart
+//    print cart
+//  else if select == view cart:
+//    print cart
+//  else if select == checkout:
+//    do checkout
+//    exit
+//  end
+
+void main() {
+  final cart = Cart();
+  while (true) {
+    stdout.write(
+        'What do you want to do? (v)iew items, (a)dd items, (c)heckout: ');
+    final line = stdin.readLineSync();
+    if (line == 'a') {
+      final product = chooseProduct();
+      if (product != null) {
+        cart.addProduct(product);
+        print(cart);
+      }
+    } else if (line == 'v') {
+      print(cart);
+    } else if (line == 'c') {
+      if (checkout(cart)) {
+        break;
+      }
+    }
+  }
+}
+
+Product? chooseProduct() {
+  final productsList =
+      allProducts.map((product) => product.diplayName).join('\n');
+  stdout.write('Available products:\n$productsList\nYour choice: ');
+  final line = stdin.readLineSync();
+  for (var product in allProducts) {
+    if (product.initial == line) {
+      return product;
+    }
+  }
+  print('Not found');
+  return null;
+}
+
+bool checkout(Cart cart) {
+  if (cart.isEmpty) {
+    print('Cart is empty');
+    return false;
+  }
+  final total = cart.total();
+  print('Total: \$$total');
+  stdout.write('Payment in cash: ');
+  final line = stdin.readLineSync();
+  if (line == null || line.isEmpty) {
+    return false;
+  }
+  final paid = double.tryParse(line);
+  if (paid == null) {
+    return false;
+  }
+  if (paid >= total) {
+    final change = paid - total;
+    print('Change: \$${change.toStringAsFixed(2)}');
+    return true;
+  } else {
+    print('Not enough cash');
+    return false;
+  }
+}
